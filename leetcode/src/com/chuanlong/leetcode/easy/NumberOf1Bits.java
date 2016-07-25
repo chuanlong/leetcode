3074a3e0510279bfe1766f7762bf00e4e47656cd
Chuanlong <xia.chuanlong@xiaoyi.com>
2016-07-25 17:49:03 +0800

    Done #191 Number of 1 Bits

---
+++ leetcode/src/com/chuanlong/leetcode/easy/NumberOf1Bits.java
@@ -0,0 +1,33 @@
+package com.chuanlong.leetcode.easy;
+
+public class NumberOf1Bits {
+
+	/**
+	 * https://leetcode.com/problems/number-of-1-bits/
+	 */
+	public static void main(String[] args) {
+
+		NumberOf1Bits obj = new NumberOf1Bits();
+		
+		System.out.println("2147483648, expected:1, result:" + obj.hammingWeight(0x80000000));
+		System.out.println("4294967295, expected:32, result:" + obj.hammingWeight(0xFFFFFFFF));
+		
+	}
+	
+    public int hammingWeight(int n) {
+    	int a = n;
+    	int count = 0;
+    	if(n < 0){
+    		a = n & 0x7FFFFFFF;
+    		count = 1;
+    	}
+    	while(a != 0){
+    		if(a%2 == 1 || a%2 == -1){
+        		count++;
+    		}
+    		a = a/2;
+    	}        
+    	return count;
+    }	
+
+}

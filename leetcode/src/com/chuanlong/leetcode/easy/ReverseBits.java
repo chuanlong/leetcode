efac55795c80569fa567b7660e234155fc067b4a
xia.chuanlong <xia.chuanlong@xiaoyi.com>
2016-07-25 23:20:35 +0800

    Done #190 Reverse Bits

---
+++ leetcode/src/com/chuanlong/leetcode/easy/ReverseBits.java
@@ -0,0 +1,30 @@
+package com.chuanlong.leetcode.easy;
+
+public class ReverseBits {
+
+	/**
+	 * https://leetcode.com/problems/reverse-bits/
+	 */
+	public static void main(String[] args) {
+
+		ReverseBits obj = new ReverseBits();		
+		
+		System.out.println("43261596, expected:964176192, result:" + obj.reverseBits(43261596));
+		
+	}
+	
+	
+    // you need treat n as an unsigned value
+    public int reverseBits(int n) {
+        int a = n;
+    	int reverse = 0;
+    	    	
+    	for(int i=0; i<32; i++){
+    		reverse = (((reverse<<1)&0xFFFFFFFE) | (a&0x00000001));
+    		a >>= 1;
+    	}
+    	
+    	return reverse;
+    }
+
+}

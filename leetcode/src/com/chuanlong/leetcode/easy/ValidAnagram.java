41dec221554c32c2811952bb4534389b79dc78f4
Chuanlong <xia.chuanlong@xiaoyi.com>
2016-07-25 10:07:43 +0800

    Done #242 Valid Anagram

---
+++ leetcode/src/com/chuanlong/leetcode/easy/ValidAnagram.java
@@ -0,0 +1,48 @@
+package com.chuanlong.leetcode.easy;
+
+public class ValidAnagram {
+
+	/**
+	 * https://leetcode.com/problems/valid-anagram/
+	 */
+	public static void main(String[] args) {
+
+		ValidAnagram obj = new ValidAnagram();
+		
+		System.out.println("[anagram, nagaram], expect:true, result:" + obj.isAnagram("anagram", "nagaram"));
+		System.out.println("[rat, car], expect:false, result:" + obj.isAnagram("rat", "car"));
+	}
+	
+	public boolean isAnagram(String s, String t) {
+		if ((s == null || "".equals(s)) && (t == null || "".equals(t))) {
+			return true;
+		}
+
+		if ((s == null || "".equals(s)) || (t == null || "".equals(t))) {
+			return false;
+		}
+
+		if (s.length() != t.length()) {
+			return false;
+		}
+		
+		int[] x = new int[26];
+		int[] y = new int[26];
+		for(int i=0; i<26; i++){
+			x[i] = y[i] = 0;
+		}
+		
+		for(int i=0; i<s.length(); i++){
+			x[s.charAt(i)-'a']++;
+			y[t.charAt(i)-'a']++;
+		}
+		
+		for(int i=0; i<x.length; i++){
+			if(x[i] != y[i]){
+				return false;
+			}
+		}
+		return true;
+	}
+	
+}

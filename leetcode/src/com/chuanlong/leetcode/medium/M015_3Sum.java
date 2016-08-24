e75416e90b715b1e7c7e714c9bb4dcff1734b408
Chuanlong <xia.chuanlong@xiaoyi.com>
2016-08-24 13:59:20 +0800

    Done #M015 3Sum

---
+++ leetcode/src/com/chuanlong/leetcode/medium/M015_3Sum.java
@@ -0,0 +1,97 @@
+package com.chuanlong.leetcode.medium;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class M015_3Sum {
+
+	/**
+	 * https://leetcode.com/problems/3sum/
+	 */
+	public static void main(String[] args) {
+
+		
+		
+	}
+	
+	
+    public List<List<Integer>> threeSum(int[] nums) {
+    	List<List<Integer>> list = new ArrayList<List<Integer>>();
+    	
+    	if(nums != null){
+    		quickSort(nums, 0, nums.length-1);
+    		
+    		for(int i=0; i<nums.length; i++){
+    			for(int j=i+1; j<nums.length; j++){
+    				
+    				int val = 0 - nums[i] - nums[j];
+    				int index = splitSearch(nums, val, j+1, nums.length-1);
+    				
+    				if(index != -1){
+    					List<Integer> item = new ArrayList<Integer>(Arrays.asList(nums[i], nums[j], nums[index]));
+    					if(!isContain(list, item)){
+        					list.add(item);
+    					}
+    				}
+    				
+    			}
+    		}
+
+    	}
+    	
+    	return list;
+    }
+
+    
+    private void quickSort(int[] nums, int start, int end){
+    	if(start < end){
+    		int key = nums[start];
+    		int i=start, j=end;
+    		while(i < j){
+    			while(i < j && nums[j] >= key) j--;
+    			nums[i] = nums[j];
+    			while(i < j && nums[i] <= key) i++;
+    			nums[j] = nums[i];
+    		}
+    		nums[i] = key;
+    		
+    		quickSort(nums, start, i-1);
+    		quickSort(nums, i+1, end);
+    	}
+    }
+    
+    private int splitSearch(int[] nums, int val, int start, int end){
+    	if(start > end){
+    		return -1;
+    	}
+    	
+    	int mid = (start + end)/2;
+    	if(nums[mid] == val){
+    		return mid;
+    	}else{
+    		if(nums[mid] < val){
+    			return splitSearch(nums, val, mid+1, end);
+    		}else{
+    			return splitSearch(nums, val, start, mid-1);
+    		}
+    	}
+    }
+    
+    private boolean isContain(List<List<Integer>> list, List<Integer> item){
+    	for(int i=0; i<list.size(); i++){
+    		boolean isSame = true;
+    		for(int j=0; j<item.size(); j++){
+    			if(list.get(i).get(j) != item.get(j)){
+    				isSame = false;
+    				break;
+    			}
+    		}
+    		if(isSame){
+    			return true;
+    		}
+    	}
+    	
+    	return false;
+    }
+}
--- leetcode/src/com/chuanlong/leetcode/util/Util.java
+++ leetcode/src/com/chuanlong/leetcode/util/Util.java
@@ -8,7 +8,12 @@ public class Util {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-
+		
+		int[] nums = new int[]{3,2,5,2,1,5,8,2,7,4};
+		System.out.print(Array2String(nums) + " -> ");
+		quickSort(nums, 0, nums.length-1);
+		System.out.println(Array2String(nums));
+		
 	}
 
 	
@@ -40,5 +45,41 @@ public class Util {
     	str += "]";
     	return str;    	
     }
+    
+    
+    public static void quickSort(int[] nums, int start, int end){
+    	if(start < end){
+    		int key = nums[start];
+    		int i=start, j=end;
+    		while(i < j){
+    			while(i < j && nums[j] >= key) j--;
+    			nums[i] = nums[j];
+    			while(i < j && nums[i] <= key) i++;
+    			nums[j] = nums[i];
+    		}
+    		nums[i] = key;
+    		
+    		quickSort(nums, start, i-1);
+    		quickSort(nums, i+1, end);
+    	}
+    }
+    
+    public static int splitSearch(int[] nums, int val, int start, int end){
+    	if(start > end){
+    		return -1;
+    	}
+    	
+    	int mid = (start + end)/2;
+    	if(nums[mid] == val){
+    		return mid;
+    	}else{
+    		if(nums[mid] < val){
+    			return splitSearch(nums, val, mid+1, end);
+    		}else{
+    			return splitSearch(nums, val, start, mid-1);
+    		}
+    	}
+    }
+    
 	
 }

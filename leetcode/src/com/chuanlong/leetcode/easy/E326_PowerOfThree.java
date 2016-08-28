d79e3715b10335c183ea3bf16e84b9b2243ee942
Chuanlong <xia.chuanlong@xiaoyi.com>
2016-08-28 11:05:23 +0800

    Done #E326 Power of Three

---
+++ leetcode/src/com/chuanlong/leetcode/easy/E326_PowerOfThree.java
@@ -0,0 +1,19 @@
+package com.chuanlong.leetcode.easy;
+
+public class E326_PowerOfThree {
+
+	/**
+	 * https://leetcode.com/problems/power-of-three/
+	 * */
+	public static void main(String[] args) {
+
+	}
+	
+	
+    public boolean isPowerOfThree(int n) {
+        return (n > 0 && 1162261467 % n == 0);
+    }
+	
+	
+
+}

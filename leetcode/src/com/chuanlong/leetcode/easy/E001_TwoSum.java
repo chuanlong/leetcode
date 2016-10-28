72aa996ff1f82a8943f684a57acbca580f5a2a4c
Chuanlong <xia.chuanlong@xiaoyi.com>
2016-10-28 16:37:15 +0800

    Done #E001 Two Sum

---
+++ leetcode/src/com/chuanlong/leetcode/easy/E001_TwoSum.java
@@ -0,0 +1,30 @@
+package com.chuanlong.leetcode.easy;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class E001_TwoSum {
+
+	/**
+	 * https://leetcode.com/problems/two-sum/
+	 */
+	public static void main(String[] args) {
+
+	}
+
+	
+	
+    public int[] twoSum(int[] nums, int target) {
+    	Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+        for(int i=0; i<nums.length; i++){
+        	if(map.containsKey(target- nums[i])){
+        		return new int[]{map.get(target-nums[i]), i};
+        	}else{
+        		map.put(nums[i], i);
+        	}
+        }
+    	return null;
+    }
+    
+    
+}

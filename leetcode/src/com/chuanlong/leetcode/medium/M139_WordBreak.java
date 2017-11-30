956ba326d41c92368cfa037473bb755389f1771e
Vincent Xia <chuanlx@amazon.com>
2017-11-30 17:30:59 +0800

    Add #M139 Word Break second solution.

--- leetcode/src/com/chuanlong/leetcode/medium/M139_WordBreak.java
+++ leetcode/src/com/chuanlong/leetcode/medium/M139_WordBreak.java
@@ -1,6 +1,8 @@
 package com.chuanlong.leetcode.medium;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class M139_WordBreak {
 	
@@ -22,5 +24,34 @@ public class M139_WordBreak {
 
     	return f[n];
     }
+    
+    public boolean wordBreak1(String s, List<String> wordDict) {
+    	return wordBreak(s, wordDict, new HashMap<String, Boolean>());
+    }
+    
+    private boolean wordBreak(String s, List<String> wordDict, Map<String, Boolean> map){
+    	if(map.containsKey(s)){
+    		return map.get(s);
+    	}
+    	
+    	Boolean isWordBreak = false;
+    	for(String str : wordDict){
+    		if(s.equals(str)){
+    			isWordBreak = true;
+    			break;
+    		}else if(str.length() < s.length() && s.startsWith(str)){
+    			Boolean isSubWordBreak = wordBreak(s.substring(str.length()), wordDict, map);
+    			if(isSubWordBreak){
+    				isWordBreak = true;
+        			break;
+    			}
+    		}
+    	}
+    	
+    	map.put(s, isWordBreak);
+    	return isWordBreak;
+    }
+    
+    
     
 }

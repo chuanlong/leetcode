8c07c1c320b43a12edc5caf14c186b20638b6b43
Vincent Xia <chuanlx@amazon.com>
2017-12-09 11:44:27 +0800

    Add #M022 Generate Parentheses another implementation.

--- leetcode/src/com/chuanlong/leetcode/medium/M022_GenerateParentheses.java
+++ leetcode/src/com/chuanlong/leetcode/medium/M022_GenerateParentheses.java
@@ -39,4 +39,29 @@ public class M022_GenerateParentheses {
     	
     }
 
+    
+    
+    public List<String> generateParenthesis1(int n) {
+        List<String> list = new ArrayList<String>();
+        generateParenthesis1(list, 0, 0, "", n);
+        return list;
+    }
+    
+    private void generateParenthesis1(List<String> list, int i, int j, String p, int n){
+    	if(i==n && j==n){
+    		list.add(p);
+    		return;
+    	}
+    	
+    	if(i<n){
+    		generateParenthesis1(list, i+1, j, p+"(", n);
+    	}
+    	if(j<n && j<i){
+    		generateParenthesis1(list, i, j+1, p+")", n);
+    	}
+    }
+    
+    
+    
+    
 }

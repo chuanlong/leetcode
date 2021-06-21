3dbd0f8d96579aa0219055edb117fdb3788cf60f
Chuanlong Xia <xcl@fb.com>
2021-06-20 23:10:41 -0700

    Redo #224. Basic Calculator

--- leetcode/src/com/chuanlong/leetcode/hard/H224_BasicCalculator.java
+++ leetcode/src/com/chuanlong/leetcode/hard/H224_BasicCalculator.java
@@ -41,4 +41,55 @@ public class H224_BasicCalculator {
     	return calc;
     }
 
+
+	public int calculate2(String s) {
+		return evaluate(s);
+	}
+
+	public int evaluate(String s) {
+    	char[] chs = s.toCharArray();
+
+    	int prev = 0;
+		int curr = 0;
+		int sign = '+';
+
+    	for(int i=0; i<chs.length; i++) {
+    		if(chs[i] >= '0' && chs[i] <= '9') {
+    			curr = curr*10 + (chs[i]-'0');
+			} else if (chs[i] == '+' || chs[i] == '-') {
+    			if(sign == '+') {
+    				prev = prev + curr;
+				} else if (sign == '-') {
+    				prev = prev - curr;
+				}
+    			sign = chs[i];
+    			curr = 0;
+			} else if (chs[i] == '(') {
+    			int end = findRightIndex(s, i);
+    			curr = evaluate(s.substring(i+1, end));
+    			i = end;
+			}
+		}
+    	if(sign == '+') {
+    		prev = prev + curr;
+		} else if (sign == '-') {
+    		prev = prev - curr;
+		}
+    	return prev;
+	}
+
+	private int findRightIndex(String s, int x) {
+    	char[] chs = s.toCharArray();
+    	int count = 1;
+    	for(int i=x+1; i<s.length(); i++) {
+    		if(chs[i] == '(') {
+    			count++;
+			} else if (chs[i] == ')') {
+    			count--;
+				if(count == 0) return i;
+    		}
+		}
+    	return -1;
+	}
+
 }

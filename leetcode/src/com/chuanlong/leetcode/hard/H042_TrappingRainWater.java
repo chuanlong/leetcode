7176c139fc49a8327f92209380817f483d796b26
Chuanlong Xia <whuxcl@gmail.com>
2022-09-11 22:12:46 -0700

    Redo #H42. Trapping Rain Water.

--- leetcode/src/com/chuanlong/leetcode/hard/H042_TrappingRainWater.java
+++ leetcode/src/com/chuanlong/leetcode/hard/H042_TrappingRainWater.java
@@ -6,11 +6,33 @@ public class H042_TrappingRainWater {
 	 * https://leetcode.com/problems/trapping-rain-water/
 	 */
 	public static void main(String[] args) {
-
+		H042_TrappingRainWater obj = new H042_TrappingRainWater();
+		System.out.println("Test1, expect:6, output:" + obj.trap(new int[]{0,1,0,2,1,0,1,3,2,1,2,1}));
 	}
+
+	public int trap(int[] height) {
+		int n = height.length;
+		int[] left = new int[n];
+		int[] right = new int[n];
+		left[0] = 0;
+		right[n-1] = 0;
+
+		for(int i=1; i<n; i++) {
+			left[i] = Math.max(left[i-1], height[i-1]);
+		}
+		for(int i=n-2; i>=0; i--) {
+			right[i] = Math.max(right[i+1], height[i+1]);
+		}
+
+		int sum = 0;
+		for(int i=1; i<n-1; i++) {
+			sum += Math.max(Math.min(left[i], right[i]) - height[i], 0);
+		}
+		return sum;
+	}
+
 	
-	
-    public int trap(int[] height) {
+    public int trap2(int[] height) {
     	if(height == null || height.length <= 2){
     		return 0;
     	}

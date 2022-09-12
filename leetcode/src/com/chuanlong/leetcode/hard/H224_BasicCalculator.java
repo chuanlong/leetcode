da44a720f8737e88bcdb491efdcda68f3db99535
Chuanlong Xia <whuxcl@gmail.com>
2022-09-12 00:24:21 -0700

    Redo #M224. Basic Calculator.

--- leetcode/src/com/chuanlong/leetcode/hard/H224_BasicCalculator.java
+++ leetcode/src/com/chuanlong/leetcode/hard/H224_BasicCalculator.java
@@ -3,8 +3,57 @@ package com.chuanlong.leetcode.hard;
 import java.util.Stack;
 
 public class H224_BasicCalculator {
-	
-    public int calculate(String s) {
+
+	public static void main(String[] args) {
+		H224_BasicCalculator obj = new H224_BasicCalculator();
+
+		System.out.println("Test1, expect:2, output:" + obj.calculate("1 + 1"));
+		System.out.println("Test2, expect:3, output:" + obj.calculate(" 2-1 + 2 "));
+		System.out.println("Test3, expect:23, output:" + obj.calculate("(1+(4+5+2)-3)+(6+8)"));
+		System.out.println("Test4, expect:3, output:" + obj.calculate("1-(         -2)"));
+		System.out.println("Test5, expect:23, output:" + obj.calculate("(1+(4+5+2)-3)+(6+8)"));
+	}
+
+
+	public int calculate(String s) {
+		Stack<Integer> stack = new Stack<>();
+		int operand = 0;
+		int result = 0;
+		int signal = 1;
+		char[] chs = s.toCharArray();
+		for(int i=0; i<chs.length; i++) {
+			char ch = chs[i];
+			if(ch >= '0' && ch <= '9') {
+				operand = operand*10 + (ch-'0');
+			} else if (ch == '+') {
+				result = result + signal*operand;
+				signal = 1;
+				operand = 0;
+			} else if (ch == '-') {
+				result = result + signal*operand;
+				signal = -1;
+				operand = 0;
+			} else if (ch == '(') {
+				stack.push(result);
+				stack.push(signal);
+				result = 0;
+				signal = 1;
+			} else if (ch == ')') {
+				int prevSignal = stack.pop();
+				int prevOperand = stack.pop();
+
+				result = prevOperand + prevSignal * (result + signal*operand);
+				signal = 1;
+				operand = 0;
+			}
+		}
+		return result + signal*operand;
+	}
+
+
+
+
+    public int calculate3(String s) {
     	if(s == null || s.length() == 0){
     		return 0;
     	}
@@ -41,12 +90,11 @@ public class H224_BasicCalculator {
     	return calc;
     }
 
-
 	public int calculate2(String s) {
-		return evaluate(s);
+		return evaluate2(s);
 	}
 
-	public int evaluate(String s) {
+	public int evaluate2(String s) {
     	char[] chs = s.toCharArray();
 
     	int prev = 0;
@@ -66,7 +114,7 @@ public class H224_BasicCalculator {
     			curr = 0;
 			} else if (chs[i] == '(') {
     			int end = findRightIndex(s, i);
-    			curr = evaluate(s.substring(i+1, end));
+    			curr = evaluate2(s.substring(i+1, end));
     			i = end;
 			}
 		}

1d1bd26e17a72a24108acc0835c645e4c2c449f2
Chuanlong Xia <whuxcl@gmail.com>
2022-09-17 20:39:23 -0700

    Redo M102. Binary Tree Level Order Traversal

--- leetcode/src/com/chuanlong/leetcode/medium/M102_BinaryTreeLevelOrderTraversal.java
+++ leetcode/src/com/chuanlong/leetcode/medium/M102_BinaryTreeLevelOrderTraversal.java
@@ -1,11 +1,13 @@
-package com.chuanlong.leetcode.easy;
+package com.chuanlong.leetcode.medium;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Queue;
+import java.util.concurrent.LinkedBlockingQueue;
 
 import com.chuanlong.leetcode.bean.TreeNode;
 
-public class E102_BinaryTreeLevelOrderTraversal {
+public class M102_BinaryTreeLevelOrderTraversal {
 
 	/**
 	 * https://leetcode.com/problems/binary-tree-level-order-traversal/
@@ -13,9 +15,27 @@ public class E102_BinaryTreeLevelOrderTraversal {
 	public static void main(String[] args) {
 
 	}
+
+	public List<List<Integer>> levelOrder(TreeNode root) {
+		List<List<Integer>> list = new ArrayList<>();
+		Queue<TreeNode> queue = new LinkedBlockingQueue<>();
+		if(root != null) queue.add(root);
+		while(!queue.isEmpty()) {
+			List<Integer> row = new ArrayList<>();
+			int size = queue.size();
+			for(int i=0; i<size; i++) {
+				TreeNode node = queue.poll();
+				row.add(node.val);
+				if(node.left != null) queue.add(node.left);
+				if(node.right != null) queue.add(node.right);
+			}
+			list.add(row);
+		}
+
+		return list;
+	}
 	
-	
-    public List<List<Integer>> levelOrder(TreeNode root) {
+    public List<List<Integer>> levelOrder1(TreeNode root) {
     	
     	List<TreeNode> order = new ArrayList<>();
     	List<Integer> level = new ArrayList<>();

df620338435d705363a96145638c5a958e0cc5d8
Chuanlong Xia <whuxcl@gmail.com>
2022-09-18 02:34:22 -0700

    Redo M46. Permutations

--- leetcode/src/com/chuanlong/leetcode/medium/M046_Permutations.java
+++ leetcode/src/com/chuanlong/leetcode/medium/M046_Permutations.java
@@ -10,6 +10,27 @@ public class M046_Permutations {
     }
 
     public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> all = new ArrayList<>();
+        backtrack(nums, new ArrayList<>(), all);
+        return all;
+    }
+
+    private void backtrack(int[] nums, List<Integer> list, List<List<Integer>> all) {
+        if(list.size() == nums.length) {
+            all.add(new ArrayList<>(list));
+            return;
+        }
+
+        int k = list.size();
+        int num = nums[k];
+        for(int i=0; i<k+1; i++) {
+            list.add(i, num);
+            backtrack(nums, list, all);
+            list.remove(i);
+        }
+    }
+
+    public List<List<Integer>> permute2(int[] nums) {
         List<List<Integer>> list = new ArrayList<>();
 
         if (nums == null || nums.length == 0) {
@@ -25,8 +46,8 @@ public class M046_Permutations {
 
         for(int i=0; i<nums.length; i++) {
             int x = nums[i];
-            int[] subNums = subArray(i, nums);
-            List<List<Integer>> subPermute = permute(subNums);
+            int[] subNums = subArray2(i, nums);
+            List<List<Integer>> subPermute = permute2(subNums);
             for(int j=0; j<subPermute.size(); j++) {
                 List<Integer> item = subPermute.get(j);
                 item.add(0, x);
@@ -37,7 +58,7 @@ public class M046_Permutations {
         return list;
     }
 
-    private int[] subArray(int index, int[] nums) {
+    private int[] subArray2(int index, int[] nums) {
         int[] subNums = new int[nums.length-1];
         for(int i=0, j=0; i<nums.length; i++) {
             if(i!=index) {

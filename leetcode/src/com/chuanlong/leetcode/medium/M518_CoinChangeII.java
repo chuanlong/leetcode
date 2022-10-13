4e6aeb2eb981de5abbcbac677649993cc8976f3d
Chuanlong Xia <whuxcl@gmail.com>
2022-10-12 22:33:46 -0700

    Done M 518. Coin Change II

---
+++ leetcode/src/com/chuanlong/leetcode/medium/M518_CoinChangeII.java
@@ -0,0 +1,19 @@
+package com.chuanlong.leetcode.medium;
+
+import java.util.Arrays;
+
+public class M518_CoinChangeII {
+
+    public int change(int amount, int[] coins) {
+        Arrays.sort(coins);
+        int[] dp = new int[amount+1];
+        dp[0] = 1;
+        for(int coin : coins) {
+            for(int i=coin; i<=amount; i++) {
+                dp[i] = dp[i] + dp[i-coin];
+            }
+        }
+        return dp[amount];
+    }
+
+}

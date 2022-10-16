c5d0f682ae49535d5bbbd2f1533674da6868fe03
Chuanlong Xia <whuxcl@gmail.com>
2022-10-16 16:09:42 -0700

    Redo M 90. Subsets II

--- leetcode/src/com/chuanlong/leetcode/medium/M090_SubsetsII.java
+++ leetcode/src/com/chuanlong/leetcode/medium/M090_SubsetsII.java
@@ -11,8 +11,34 @@ public class M090_SubsetsII {
 
 	}
 	
+	public List<List<Integer>> subsetsWithDup(int[] nums) {
+		Map<Integer, Integer> map = new HashMap<>();
+		for(int num : nums) {
+			int cnt = map.getOrDefault(num, 0);
+			map.put(num, cnt+1);
+		}
+		List<Integer> keys = new ArrayList<>(map.keySet());
+
+		List<List<Integer>> result = new ArrayList<>();
+		result.add(new ArrayList<>());
+		backtrack(keys, map, 0, new ArrayList<>(), result);
+		return result;
+	}
+
+	private void backtrack(List<Integer> keys, Map<Integer, Integer> map, int index, List<Integer> item, List<List<Integer>> result) {
+		if(index >= keys.size()) return;
+		backtrack(keys, map, index+1, item, result);
+		int key = keys.get(index);
+		int cnt = map.get(key);
+		for(int i=1; i<=cnt; i++) {
+			item.add(key);
+			result.add(new ArrayList<>(item));
+			backtrack(keys, map, index+1, item, result);
+		}
+		for(int i=1; i<=cnt; i++) item.remove(item.size()-1);
+	}
 	
-    public List<List<Integer>> subsetsWithDup(int[] nums) {
+    public List<List<Integer>> subsetsWithDup2(int[] nums) {
         Map<Integer, Integer> map = new HashMap<Integer, Integer>();
         if(nums != null && nums.length > 0){
         	for(int i=0; i<nums.length; i++){
